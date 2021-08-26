33ecd4d6eaf400b087f910a551ed29d4c2a4b888
Matt Haddock <777mth@gmail.com>
2021-08-26 00:22:42 -0400

    second commit

--- public/start-electron.js
+++ public/start-electron.js
@@ -1,22 +1,55 @@
-const electron = require('electron'),
-  app = electron.app,
-  BrowserWindow = electron.BrowserWindow;
-   
-const path = require('path'),
-  isDev = require('electron-is-dev');
-   
+const path = require("path");
+
+const { app, BrowserWindow } = require("electron");
+const isDev = require("electron-is-dev");
+
+// Conditionally include the dev tools installer to load React Dev Tools
+let installExtension, REACT_DEVELOPER_TOOLS;
+if (isDev) {
+  const devTools = require("electron-devtools-installer");
+  installExtension = devTools.default;
+  REACT_DEVELOPER_TOOLS = devTools.REACT_DEVELOPER_TOOLS;
+}
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling
+if (require("electron-squirrel-startup")) {
+  app.quit();
+}
+
 let mainWindow;
    
 const createWindow = () => {
-  mainWindow = new BrowserWindow({ width: 480, height: 320 })
+  mainWindow = new BrowserWindow({ 
+    width: 480,
+    height: 320,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  
   const appUrl = isDev ? 'http://localhost:3000' :
     `file://${path.join(__dirname, '../build/index.html')}`
   mainWindow.loadURL(appUrl)
-  mainWindow.maximize()
-  mainWindow.setFullScreen(true)
+  // mainWindow.maximize()
+  mainWindow.setFullScreen(false)
+
+  // Open the DevTools.
+  if (isDev) {
+    win.webContents.openDevTools({ mode: "detach" });
+  }
+
   mainWindow.on('closed', () => mainWindow = null)
+
 }
-app.on('ready', createWindow)
+app.whenReady().then(() => {
+  createWindow();
+
+  if (isDev) {
+    installExtension(REACT_DEVELOPER_TOOLS)
+      .then(name => console.log(`Added Extension: ${name}`))
+      .catch(error => console.log(`An error occurred: , ${error}`))
+  }
+});
 app.on('window-all-closed', () => {
   // Follow OS convention on whether to quit app when
   // all windows are closed.
